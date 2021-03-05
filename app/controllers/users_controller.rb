11b9dd40fd51dcfb6af128194e599d43c6e9b466
Wesley <wspencer3295@yahoo.com>
2021-03-04 21:36:17 -0700

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :github)
+      params.require(:user).permit(:first_name, :last_name,:email, :github, :linkdn, :facebook, :twitter, :reddit, :youtube)
     end
 end
