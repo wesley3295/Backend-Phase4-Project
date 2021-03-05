9a6826d53b876a948442169db0744ea4575bfcc4
Wesley <wspencer3295@yahoo.com>
2021-03-04 21:36:39 -0700

    +validations user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :projects
     validates :first_name, presence: true
     validates :last_name, presence: true
+    validates :github, presence: true
+    validates :email, presence: true
     
 
 end
